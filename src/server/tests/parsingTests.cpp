81d6d03490c47793836aa43b8295b18e154ff308
Alicja Kluczek <alicja2602@gmail.com>
2021-04-26 00:37:13 +0200

    Prepare Moodle version.

--- src/server/tests/parsingTests.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-#include "../connectionHandler.h"
-
-TEST(correlated_files_parsing, simple) {
-    auto c = CorrelatedFile("plik\t127.0.0.1\t8080");
-    ASSERT_EQ(c.port, "8080");
-    ASSERT_EQ(c.ipAddress, "127.0.0.1");
-    ASSERT_EQ(c.resource, "plik");
-}
